1c8adbe6ca61f497a20f17dd2e7ade48248d3887
Antoine Hogenboom <antoinehbm@gmail.com>
2020-03-31 16:36:45 +0200

    Klooien met kolommen

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,16 +3,36 @@
 <main>
   <div class="row align-items-center pt-5 pb-5">
     
-    <div class="col-12 col-lg-6">
-      <img src="<?= $page->image()->url(); ?>" class="img-fluid" alt="<?= $page->image()->title(); ?>" />
-    </div>
-    <div class="col-12 col-lg-6">
+  <div class="col-12 col-lg-6">
       <h1><?= $page->introductiontitle() ?></h1>
       <p class="lead"><?= $page->introductiontext() ?></p>
       <a href="<?= $site->page('projects') ?>" class="btn btn-primary" role="button" aria-pressed="true">View my work</a>
       <a href="<?= $site->page('about') ?>" class="btn btn-outline-primary" role="button" aria-pressed="true">More about me</a>
     </div>
+
+    <div class="col-12 col-lg-6">
+      <img src="<?= $page->image()->url(); ?>" class="img-fluid" alt="<?= $page->image()->title(); ?>" />
+    </div>
   </div>
+
+
+  <div class="row">
+    <div class="col-6">
+      <div class="row">
+        <div class="col-6">
+          <p>Dit is dummytekst blablabla laalal ieodjoswde  iop;ueadh oiu adeuio aesdoiua </p>
+        </div>
+        <div class="col-6">
+          <p>Dit is dummytekst blablabla. Laten we er voor de verandering eens even gewone tekst van maken, kijken of dat werkt. ja leuk doei...</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-6">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, sit molestiae nobis quidem dicta Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit aperiam quia, ducimus aut incidunt eum voluptates, soluta eligendi quod beatae corrupti cupiditate consequuntur facilis veritatis aliquid quos quaerat possimus. </p>
+    </div>
+  </div>
+
+
   <?php
   if ($projectsPage = page('projects')): ?>
   <div class="row border-top pt-5 pb-5">
@@ -20,8 +40,8 @@
       <h2 class="float-left pb-4">My work</h2>
       <a class="float-right projectsknop" href="<?= $site->page('projects') ?>">View all projects &#8594;</a>
     </div>
-    <?php foreach ($projectsPage->children()->listed()->paginate(3) as $project): ?>
-    <div class="col-12 col-lg-4 mb-4">
+    <?php foreach ($projectsPage->children()->listed()->paginate(4) as $project): ?>
+    <div class="col-12 col-lg-3 mb-4">
       <div class="card h-100">
       <img src="<?= $project->images()->findBy("template", "thumbnail")->url(); ?>" class="card-img-top" alt="thumbnail" />
       <div class="card-body">
